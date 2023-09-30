25a500efa157283486aa016e6916d21012fba244
Valery <Valery@HOME-PC>
2023-09-30 19:04:46 +0300

    1

---
+++ Lesson_1_task_1/Lesson_1_task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("write n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int d = 0;
+            int i = 2;
+            if (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                else
+                {
+                    i++;
+                }
+                if (d == 0)
+                {
+                    Console.WriteLine("prostoe");
+                }
+                else
+                {
+                    Console.WriteLine("not prostoe");
+                }
+            }
+        }
+    }
+}
