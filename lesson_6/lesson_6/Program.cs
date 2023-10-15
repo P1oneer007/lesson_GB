b12bb30e5db2ffea1e4b45ff2198ff46c5f3b59c
Valery <Valery@HOME-PC>
2023-10-15 20:14:04 +0300

    3

---
+++ lesson_6/lesson_6/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Graph
+{
+    private int V; // Количество вершин
+    private List<int>[] adjacencyList; // Список смежности
+
+    public Graph(int v)
+    {
+        V = v;
+        adjacencyList = new List<int>[V];
+        for (int i = 0; i < V; i++)
+        {
+            adjacencyList[i] = new List<int>();
+        }
+    }
+
+    public void AddEdge(int v, int w)
+    {
+        adjacencyList[v].Add(w);
+    }
+
+    public void DFS(int startVertex)
+    {
+        bool[] visited = new bool[V];
+        DFSUtil(startVertex, visited);
+    }
+
+    private void DFSUtil(int vertex, bool[] visited)
+    {
+        visited[vertex] = true;
+        Console.Write(vertex + " ");
+
+        foreach (int adjacentVertex in adjacencyList[vertex])
+        {
+            if (!visited[adjacentVertex])
+            {
+                DFSUtil(adjacentVertex, visited);
+            }
+        }
+    }
+
+    public void BFS(int startVertex)
+    {
+        bool[] visited = new bool[V];
+        Queue<int> queue = new Queue<int>();
+
+        visited[startVertex] = true;
+        queue.Enqueue(startVertex);
+
+        while (queue.Count > 0)
+        {
+            int vertex = queue.Dequeue();
+            Console.Write(vertex + " ");
+
+            foreach (int adjacentVertex in adjacencyList[vertex])
+            {
+                if (!visited[adjacentVertex])
+                {
+                    visited[adjacentVertex] = true;
+                    queue.Enqueue(adjacentVertex);
+                }
+            }
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int vertices = 6;
+        Graph graph = new Graph(vertices);
+
+        graph.AddEdge(0, 1);
+        graph.AddEdge(0, 2);
+        graph.AddEdge(1, 3);
+        graph.AddEdge(1, 4);
+        graph.AddEdge(2, 4);
+        graph.AddEdge(3, 4);
+        graph.AddEdge(3, 5);
+        graph.AddEdge(4, 5);
+
+        Console.WriteLine("DFS обход:");
+        graph.DFS(0);
+        Console.WriteLine();
+
+        Console.WriteLine("BFS обход:");
+        graph.BFS(0);
+        Console.WriteLine();
+    }
+}
---
+++ lesson_7/lesson_7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int M = 3; // количество строк
+        int N = 4; // количество столбцов
+
+        int count = CountPaths(M, N);
+        Console.WriteLine("Количество путей: " + count);
+    }
+
+    static int CountPaths(int m, int n)
+    {
+        int[,] dp = new int[m, n];
+
+        // Инициализация первого столбца
+        for (int i = 0; i < m; i++)
+        {
+            dp[i, 0] = 1;
+        }
+
+        // Инициализация первой строки
+        for (int j = 0; j < n; j++)
+        {
+            dp[0, j] = 1;
+        }
+
+        // Заполнение остальных клеток
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+            }
+        }
+
+        return dp[m - 1, n - 1];
+    }
+}
